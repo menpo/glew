c253d5e1e5ac62e2eefd19b83ece47880c53eb8d
Patrick Snape <patricksnape@gmail.com>
2014-10-11 21:09:14 +0100

    OK, let's try not setting the generator on Win

--- conda/bld.bat
+++ conda/bld.bat
@@ -5,22 +5,14 @@ robocopy %RECIPE_DIR%\.. . /E /NFL
 mkdir build
 cd build
 
-rem Need to handle Python 3.x case at some point (Visual Studio 2010)
 if %ARCH%==32 (
-  call "C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\vcvarsall.bat" x86
-  if %PY_VER% LSS 3 (
-    set CMAKE_GENERATOR="Visual Studio 9 2008"
     set CMAKE_CONFIG="Release|Win32"
-  )
 )
 if %ARCH%==64 (
-  if %PY_VER% LSS 3 (
-    set CMAKE_GENERATOR="Visual Studio 9 2008 Win64"
     set CMAKE_CONFIG="Release|x64"
-  )
 )
 
-cmake .. -G%CMAKE_GENERATOR% ^
+cmake .. ^
  -DBUILD_SHARED_LIBS=1 ^
  -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX%
 
