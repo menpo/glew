c709bb50136aeab8ed2ef829217a29b9d5cb133d
Patrick Snape <patricksnape@gmail.com>
2014-10-11 21:49:44 +0100

    OK, we do need the generator else we V2012

--- conda/bld.bat
+++ conda/bld.bat
@@ -1,20 +1,23 @@
-@echo off
-
 robocopy %RECIPE_DIR%\.. . /E /NFL
 
 mkdir build
 cd build
 
+rem Need to handle Python 3.x case at some point (Visual Studio 2010)
 if %ARCH%==32 (
-    set VS110COMNTOOLS=""
-    set VS120COMNTOOLS=""
+  if %PY_VER% LSS 3 (
+    set CMAKE_GENERATOR="Visual Studio 9 2008"
     set CMAKE_CONFIG="Release"
+  )
 )
 if %ARCH%==64 (
+  if %PY_VER% LSS 3 (
+    set CMAKE_GENERATOR="Visual Studio 9 2008 Win64"
     set CMAKE_CONFIG="Release"
+  )
 )
 
-cmake .. ^
+cmake .. -G"%CMAKE_GENERATOR%" ^
  -DBUILD_SHARED_LIBS=1 ^
  -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX%
 
